f9136450b783f8e67ec7be153a741c02e0724630
EzeSar <eservicesupply@gmail.com>
2023-05-24 02:59:43 -0300

    “Agrego

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,5 @@
+let gradosF = 75;
+let gradosC = (gradosF - 32) * 5 / 9;
+console.log(gradosC);
+//dado el ejemplo de 50 grados Fahrenheit resulta en 10 grados Celsius
+//dado el ejemplo de 75 grados Fahrenheit resulta en 23.88888888888889 grados Celsius
