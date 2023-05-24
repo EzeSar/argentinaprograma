a8304eb3f70aeab9029714c9b32aef546fcaed52
EzeSar <eservicesupply@gmail.com>
2023-05-24 02:57:15 -0300

    agrego los archivos resta.js y multiplicacion.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,6 @@
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+let multiplicacion = num1 * num2 * num3;
+console.log(multiplicacion)
+//dado el ejemplo de multiplicar los numeros 2,17,6 el resultado es 204
---
+++ practico0/resta.js
@@ -0,0 +1,5 @@
+let minuendo = 10;
+let sustraendo = 2;
+let resta = minuendo - sustraendo;
+console.log(resta);
+//dado el ejemplo minuendo = 10 y sustraendo = 2 el resultado resta es 8
