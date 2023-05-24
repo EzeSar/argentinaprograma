f967cc756c63c6183e82bde1d3960cc5ed74c156
EzeSar <eservicesupply@gmail.com>
2023-05-24 03:06:19 -0300

    practico n0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,6 @@
+let base = 12.5;
+let altura = 5.3;
+let area = base * altura;
+console.log(area)
+//dado el ejemplo de base 5 y altura 3 el resultado es 15
+//dado el ejemplo de base 12.5 y altura 5.3 el resultado es 66.25
