feb133637b3a0cff5986c62d9f7f80adc46cfa05
EzeSar <eservicesupply@gmail.com>
2023-05-27 12:32:05 -0300

    Practico0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -4,3 +4,4 @@ let area = base * altura;
 console.log(area)
 //dado el ejemplo de base 5 y altura 3 el resultado es 15
 //dado el ejemplo de base 12.5 y altura 5.3 el resultado es 66.25
+//
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -3,6 +3,6 @@ let num2 = 5.3;
 let num3 = -3.3;
 let promedio3 = (num1+num2+num3) / 3;
 console.log(promedio3);
-//dado el ejemplo de los numeros 5,7,9 el promedio resulta en 7
-//dado el ejemplo de los numeros -2,-3,-8 el promedio resulta en -4.333333333333333
-//dado el ejemplo de los numeros -1.2,5.3,-3.3 el promedio resulta en 0.2666666666666666
+//dado los numeros 5,7,9 el promedio resulta en 7
+//dado los numeros -2,-3,-8 el promedio resulta en -4.333333333333333
+//dado los numeros -1.2,5.3,-3.3 el promedio resulta en 0.2666666666666666
